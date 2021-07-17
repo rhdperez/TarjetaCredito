4e7cb738fa86c9150455999e31040bcf4d041836
Daniel_LPT <rhdperez@hotmail.com>
2021-07-17 09:02:41 -0300

    fronted listo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +15,10 @@ import { TarjetaCreditoComponent } from './components/tarjeta-credito/tarjeta-cr
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-tarjeta-credito',
@@ -6,10 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tarjeta-credito.component.css']
 })
 export class TarjetaCreditoComponent implements OnInit {
+  listTarjetas: any[] = [
+    {titular: 'Juan Perez', numeroTarjeta: '1234654398761598', fechaExpiracion: '11/21', cvv:'123' },
+    {titular: 'Miguel Gonzalez', numeroTarjeta: '1234665988761598', fechaExpiracion: '02/21', cvv:'123'},
+  ];
 
-  constructor() { }
+  form: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder,
+    private toastr: ToastrService) {
+    this.form = this.fb.group({
+      titular: ['', Validators.required],
+      numeroTarjeta: ['', [Validators.required, Validators.maxLength(16), Validators.minLength(16)]],
+      fechaExpiracion: ['', [Validators.required, Validators.maxLength(5), Validators.minLength(5)]],
+      cvv: ['', [Validators.required, Validators.maxLength(3), Validators.minLength(3)]]
+    })
+   }
+
+   ngOnInit(): void {
+  }
+  //ACA CREO EL OBJ QUE TENDRÁ LOS DATOS DE LA TARJETA INGRESADA EN EL FORMULARIO Y LO IMPRIMO
+  agregarTarjeta(){
+     const tarjeta: any = {
+       titular: this.form.get('titular')?.value,
+       numeroTarjeta: this.form.get('numeroTarjeta')?.value,
+       fechaExpiracion: this.form.get('fechaExpiracion')?.value,
+       cvv: this.form.get('cvv')?.value,
+     }
+     //console.log(tarjeta);
+     this.listTarjetas.push(tarjeta);
+     this.toastr.success('La Tarjeta fue registrada con éxito', 'Tarjeta Registrada');
+     this.form.reset();
+  }
+
+  eliminarTarjeta(index: number){
+    this.listTarjetas.splice(index,1);
+    this.toastr.error('La Tarjeta fue eliminada con éxito', 'Tarjeta Eliminada');
   }
 
 }
